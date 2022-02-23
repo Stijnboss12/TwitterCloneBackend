5f8148ac97d774529b18aabe1f6001529acf501c
Stijn van Oudheusden <434876@student.fontys.nl>
2022-02-23 09:20:34 +0100

    Update WeatherForecastController.cs

--- TwitterCloneBackend/Controllers/WeatherForecastController.cs
+++ TwitterCloneBackend/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace TwitterCloneBackend.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Milde", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;

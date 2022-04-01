38be1d80475e0007e9963ab06c1046037a989729
Stijn <434876@student.fontys.nl>
2022-04-01 13:35:34 +0200

    add mapper and method

--- UserMicroService/Services/UserService.cs
+++ UserMicroService/Services/UserService.cs
@@ -1,4 +1,7 @@
-﻿using UserMicroService.Repositories.Interfaces;
+﻿using AutoMapper;
+using UserMicroService.Models;
+using UserMicroService.Models.DTO;
+using UserMicroService.Repositories.Interfaces;
 using UserMicroService.Services.Interfaces;
 
 namespace UserMicroService.Services
@@ -6,9 +9,25 @@ namespace UserMicroService.Services
     public class UserService : IUserService
     {
         private readonly IUserRepository _userRepository;
-        public UserService(IUserRepository userRepository)
+        private readonly IMapper _mapper;
+        public UserService(IUserRepository userRepository, IMapper mapper)
         {
             _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<User> UserLogin(UserDTO userDTO)
+        {
+            var loggedinUser = await _userRepository.GetUser(userDTO.Id);
+
+            if (loggedinUser.Id != string.Empty)
+            {
+                return loggedinUser;
+            }
+
+            var createdUser = await _userRepository.CreateUser(_mapper.Map<User>(userDTO));
+
+            return createdUser;
         }
     }
 }

8297667f4275e97797d28bc624223ef27c0190c1
Stijn <434876@student.fontys.nl>
2022-04-01 13:35:10 +0200

    add methods

--- UserMicroService/Repositories/Interfaces/IUserRepository.cs
+++ UserMicroService/Repositories/Interfaces/IUserRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace UserMicroService.Repositories.Interfaces
+﻿using UserMicroService.Models;
+
+namespace UserMicroService.Repositories.Interfaces
 {
     public interface IUserRepository
     {
+        public Task<User> GetUser(string id);
+        public Task<User> CreateUser(User newUser);
     }
 }

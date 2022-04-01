87302dfa195854824f9ed83463e1e847b9694e13
Stijn <434876@student.fontys.nl>
2022-04-01 13:35:15 +0200

    add methods

--- UserMicroService/Repositories/UserRepository.cs
+++ UserMicroService/Repositories/UserRepository.cs
@@ -1,4 +1,9 @@
-﻿using UserMicroService.Data;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using UserMicroService.Data;
+using UserMicroService.Models;
+using UserMicroService.Models.DTO;
 using UserMicroService.Repositories.Interfaces;
 
 namespace UserMicroService.Repositories
@@ -10,6 +15,26 @@ namespace UserMicroService.Repositories
         public UserRepository(UserDbContext userDbContext)
         {
             _dbContext = userDbContext;
+        }
+
+        public async Task<User> GetUser(string id)
+        {
+            return await _dbContext.Users.Where(x => x.Id == id).FirstOrDefaultAsync() ?? new User();
+        }
+
+        public async Task<User> CreateUser(User newUser)
+        {
+            var user = await GetUser(newUser.Id);
+
+            if (user.Id != string.Empty)
+            {
+                throw new DbUpdateException($"A user with id:{newUser.Id} already exists");
+            }
+
+            await _dbContext.Users.AddAsync(newUser);
+            await _dbContext.SaveChangesAsync();
+
+            return newUser;
         }
     }
 }

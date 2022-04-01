0f400676478afdedf68002f55bb48dd5cd64f349
Stijn <434876@student.fontys.nl>
2022-04-01 13:34:53 +0200

    add model

--- UserMicroService/Models/User.cs
+++ UserMicroService/Models/User.cs
@@ -1,6 +1,12 @@
-﻿namespace UserMicroService.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserMicroService.Models
 {
     public class User
     {
+        [Required]
+        public string Id { get; set; } = string.Empty;
+        [Required]
+        public string Username { get; set; } = string.Empty;
     }
 }

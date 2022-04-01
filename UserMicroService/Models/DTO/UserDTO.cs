23bed137ea216debb02676d74b21ee3bb66b58f4
Stijn <434876@student.fontys.nl>
2022-04-01 13:34:48 +0200

    add dto

--- UserMicroService/Models/DTO/UserDTO.cs
+++ UserMicroService/Models/DTO/UserDTO.cs
@@ -1,6 +1,12 @@
-﻿namespace UserMicroService.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserMicroService.Models.DTO
 {
     public class UserDTO
     {
+        [Required]
+        public string Id { get; set; } = string.Empty;
+        [Required]
+        public string Username { get; set; } = string.Empty;
     }
 }

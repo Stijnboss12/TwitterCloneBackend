83b7217cefd9b0e62fd935dfd7d90cb5c3d8c2b3
Stijn <434876@student.fontys.nl>
2022-04-01 13:34:16 +0200

    add model

--- PostMicroService/Models/Post.cs
+++ PostMicroService/Models/Post.cs
@@ -1,6 +1,14 @@
-﻿namespace PostMicroService.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PostMicroService.Models
 {
     public class Post
     {
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        public Guid UserId { get; set; }
+        [Required]
+        public string Message { get; set; }
     }
 }

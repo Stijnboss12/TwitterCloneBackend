599a3f1ab9ffef7f73366b6fd14568e23ad258f0
Stijn <434876@student.fontys.nl>
2022-04-01 13:35:03 +0200

    add mapper and remove cors

--- UserMicroService/Program.cs
+++ UserMicroService/Program.cs
@@ -4,13 +4,29 @@ using UserMicroService.Repositories.Interfaces;
 using UserMicroService.Services;
 using UserMicroService.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Diagnostics;
+using UserMicroService.Domain;
+using System.Text;
+using AutoMapper;
+using UserMicroService.Models.DTO;
+using UserMicroService.Models;
 
-var MyCors = "_myCors";
+IMapper SetupMapper()
+{
+    var configuration = new MapperConfiguration(cfg =>
+    {
+        cfg.CreateMap<UserDTO, User>();
+        cfg.CreateMap<User, UserDTO>();
+    });
+
+    return new Mapper(configuration);
+}
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddSingleton<IMapper>(x => SetupMapper());
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -23,17 +39,6 @@ builder.Services.AddScoped<IUserService, UserService>();
 // Setup database
 builder.Services.AddDbContext<UserDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: MyCors,
-                      builder =>
-                      {
-                          builder.WithOrigins("http://localhost:8080")
-                          .AllowAnyHeader()
-                          .AllowAnyMethod();
-                      });
-});
 
 var app = builder.Build();
 
@@ -52,13 +57,31 @@ using (var scope = app.Services.CreateScope())
     context.Database.EnsureCreated();
 }
 
+app.UseExceptionHandler(errorApp =>
+{
+    errorApp.Run(async context =>
+    {
+        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        context.Response.ContentType = "application/json";
+
+        var error = context.Features.Get<IExceptionHandlerFeature>();
+        if (error != null)
+        {
+            var ex = error.Error;
+
+            await context.Response.WriteAsync(new IError()
+            {
+                StatusCode = 400,
+                Message = ex.Message
+            }.ToString(), Encoding.UTF8);
+        }
+    });
+});
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.UseCors(MyCors);
-
 app.MapControllers();
 
 app.Run();
-

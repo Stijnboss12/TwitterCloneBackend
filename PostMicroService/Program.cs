4e4b49f6518e683c3b049700df909ab2deec90f4
Stijn <434876@student.fontys.nl>
2022-04-07 12:26:23 +0200

    add mapper and use middleware

--- PostMicroService/Program.cs
+++ PostMicroService/Program.cs
@@ -4,11 +4,28 @@ using PostMicroService.Repositories.Interfaces;
 using PostMicroService.Services;
 using PostMicroService.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Diagnostics;
+using PostMicroService.Domain;
+using System.Text;
+using AutoMapper;
+using PostMicroService.Models.DTO;
+using PostMicroService.Models;
+
+IMapper SetupMapper()
+{
+    var configuration = new MapperConfiguration(cfg =>
+    {
+        cfg.CreateMap<PostDTO, Post>();
+        cfg.CreateMap<Post, PostDTO>();
+    });
+
+    return new Mapper(configuration);
+}
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddSingleton<IMapper>(x => SetupMapper());
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -38,6 +55,8 @@ using (var scope = app.Services.CreateScope())
     var context = services.GetRequiredService<PostDbContext>();
     context.Database.EnsureCreated();
 }
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseHttpsRedirection();
 

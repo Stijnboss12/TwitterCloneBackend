8c2d2311a3b60f4ec2e991bf56e17f56ad4e1096
Stijn <434876@student.fontys.nl>
2022-04-07 12:25:14 +0200

    update post dto

--- PostMicroService/Models/DTO/PostDTO.cs
+++ PostMicroService/Models/DTO/PostDTO.cs
@@ -1,6 +1,13 @@
-﻿namespace PostMicroService.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PostMicroService.Models.DTO
 {
     public class PostDTO
     {
+        public Guid Id { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(300)]
+        public string Content { get; set; } = string.Empty;
     }
 }

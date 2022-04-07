a87bfe588cbcdd021c5819554706677b6bab6e58
Stijn <434876@student.fontys.nl>
2022-04-07 12:26:51 +0200

    add get and create methods to postservice

--- PostMicroService/Services/Interfaces/IPostService.cs
+++ PostMicroService/Services/Interfaces/IPostService.cs
@@ -1,6 +1,11 @@
-﻿namespace PostMicroService.Services.Interfaces
+﻿using PostMicroService.Models;
+using PostMicroService.Models.DTO;
+
+namespace PostMicroService.Services.Interfaces
 {
     public interface IPostService
     {
+        public Task<List<Post>> GetPosts();
+        public Task<Post> CreateNewPost(PostDTO postDTO);
     }
 }

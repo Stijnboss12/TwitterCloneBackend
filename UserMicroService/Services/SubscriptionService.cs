fd1e1910957d43dd6016afae6aaa7a51e3f0535d
Stijn <434876@student.fontys.nl>
2022-04-15 14:17:33 +0200

    add subscriptionservice and methods

---
+++ UserMicroService/Services/SubscriptionService.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using UserMicroService.Models;
+using UserMicroService.Models.DTO;
+using UserMicroService.Repositories.Interfaces;
+using UserMicroService.Services.Interfaces;
+
+namespace UserMicroService.Services
+{
+    public class SubscriptionService : ISubscriptionService
+    {
+        private readonly ISubscriptionRepository _subscriptionRepository;
+        private readonly IMapper _mapper;
+        public SubscriptionService(ISubscriptionRepository subscriptionRepository, IMapper mapper)
+        {
+            _subscriptionRepository = subscriptionRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Subscription> CreateSubscription(SubscriptionDTO newSubscriptionDTO)
+        {
+            var existingSubscription = await _subscriptionRepository.GetSubscription(_mapper.Map<Subscription>(newSubscriptionDTO));
+            if (existingSubscription.Id != Guid.Empty)
+            {
+                return existingSubscription;
+            }
+
+            var createdSubscription = await _subscriptionRepository.CreateSubscription(_mapper.Map<Subscription>(newSubscriptionDTO));
+
+            return createdSubscription;
+        }
+
+        public async Task<Subscription> RemoveSubscription(SubscriptionDTO subscriptionDTO)
+        {
+            var subscriptionToDelete = _mapper.Map<Subscription>(subscriptionDTO);
+
+            var deletedSubscription = await _subscriptionRepository.RemoveSubscription(subscriptionToDelete);
+
+            return deletedSubscription;
+        }
+    }
+}

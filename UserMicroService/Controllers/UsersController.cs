cdf4e9b1296863024c1d5e05aa81c945467cd803
Stijn <434876@student.fontys.nl>
2022-04-15 14:16:30 +0200

    add subscription service and new endpoints

--- UserMicroService/Controllers/UsersController.cs
+++ UserMicroService/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using UserMicroService.Models;
 using UserMicroService.Models.DTO;
 using UserMicroService.Services.Interfaces;
 
@@ -9,9 +10,12 @@ namespace UserMicroService.Controllers
     public class UsersController : ControllerBase
     {
         private readonly IUserService _userService;
-        public UsersController(IUserService userService)
+        private readonly ISubscriptionService _subscriptionService;
+
+        public UsersController(IUserService userService, ISubscriptionService subscriptionService)
         {
             _userService = userService;
+            _subscriptionService = subscriptionService;
         }
 
         [HttpGet("Login")]
@@ -24,6 +28,34 @@ namespace UserMicroService.Controllers
             var loggedinuser = await _userService.UserLogin(user);
 
             return Ok();
+        }
+
+        [HttpGet("{username}")]
+        public async Task<IActionResult> GetUsers(string username)
+        {
+            if (username == string.Empty)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var users = await _userService.GetUsersByUsername(username);
+
+            return Ok(users);
+        }
+
+        [HttpPost("Subscribe")]
+        public async Task<IActionResult> SubscribeToUser([FromBody] SubscriptionDTO subscriptionDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            subscriptionDTO.SubscriberId = Request.Headers.ToDictionary(x => x.Key, x => x.Value)["id"];
+
+            var createdSubscription = await _subscriptionService.CreateSubscription(subscriptionDTO);
+
+            return Ok(createdSubscription);
         }
     }
 }
